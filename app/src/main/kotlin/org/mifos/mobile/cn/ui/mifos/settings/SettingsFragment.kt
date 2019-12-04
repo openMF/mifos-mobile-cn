3a2f68ac58c6ed16c99ef57d6ac1ebfdc624bb1b
PiyushSharma99 <Grtpiyush@gmail.com>
2019-12-04 08:49:29 +0530

    Handled Settings click in Nav drawer and seperate fragment for it

---
+++ app/src/main/kotlin/org/mifos/mobile/cn/ui/mifos/settings/SettingsFragment.kt
@@ -0,0 +1,41 @@
+package org.mifos.mobile.cn.ui.mifos.settings
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.mifos.mobile.cn.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [SettingsFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [SettingsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    companion object {
+        fun newInstance(): SettingsFragment {
+            val fragment = SettingsFragment()
+            val args = Bundle()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
