3c66cf21b2e2ca2fa8da0820b8ef964d28de6e91
Rachit Gupta <guptarachit2004@gmail.com>
2020-01-18 22:50:15 +0530

    redesign: transfer and amount screen

---
+++ app/src/main/kotlin/org/mifos/mobile/cn/ui/mifos/amount/AmountActivity.kt
@@ -0,0 +1,13 @@
+package org.mifos.mobile.cn.ui.mifos.amount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.mifos.mobile.cn.R
+
+class AmountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_amount)
+    }
+}
