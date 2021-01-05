96a1e3075f31525c895d5e415acf55a717e909e9
Prashant830 <ps962630@gmail.com>
2021-01-05 17:15:36 +0530

    fix #169 Settings is empty and white

--- app/src/main/kotlin/org/mifos/mobile/cn/ui/mifos/settings/SettingsFragment.kt
+++ app/src/main/kotlin/org/mifos/mobile/cn/ui/mifos/settings/SettingsFragment.kt
@@ -1,41 +1,27 @@
 package org.mifos.mobile.cn.ui.mifos.settings
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
 import org.mifos.mobile.cn.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class SettingsActivity : AppCompatActivity() {
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [SettingsFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SettingsFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_settings)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.settings, SettingsFragment())
+                    .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    companion object {
-        fun newInstance(): SettingsFragment {
-            val fragment = SettingsFragment()
-            val args = Bundle()
-            fragment.arguments = args
-            return fragment
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.setting_preference, rootKey)
         }
     }
 }
