10717c6ca0693504b4b8e4769a9f5e5997ce80c5
Prashant Singh <ps962630@gmail.com>
2021-01-27 18:00:29 +0530

    fixed #213 At sign up activity no option for already have account?

--- app/src/main/kotlin/org/mifos/mobile/cn/ui/mifos/signup/SignupActivity.kt
+++ app/src/main/kotlin/org/mifos/mobile/cn/ui/mifos/signup/SignupActivity.kt
@@ -1,15 +1,28 @@
 package org.mifos.mobile.cn.ui.mifos.signup
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_login.*
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_signup.*
 import org.mifos.mobile.cn.R
 import org.mifos.mobile.cn.ui.base.MifosBaseActivity
-import org.mifos.mobile.cn.ui.mifos.login.LoginContract
+import org.mifos.mobile.cn.ui.mifos.login.LoginActivity
 
 class SignupActivity: MifosBaseActivity(){
+    var login: TextView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        login = findViewById(R.id.already_have_an_account)
+
+        already_have_an_account.setOnClickListener(View.OnClickListener {
+            val R = Intent(this@SignupActivity, LoginActivity::class.java)
+            startActivity(R)
+            finish()
+        })
+
     }
 }
